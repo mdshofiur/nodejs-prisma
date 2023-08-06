2fb65828dfe3ec782c40bf3bf00f8ab7493b4d1d
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-08-06 16:00:51 +0600

    prisma config added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 dotenv.config();
 
@@ -10,6 +13,39 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
   
 });
+
+
+  async function main() {
+    // await prisma.user.create({
+    //   data: {
+    //     name: 'Alice',
+    //     email: 'alicae@prisma.io',
+    //     password: 'password'
+    //   },
+    // })
+  
+    
+
+    const allUsers = await prisma.user.findMany()
+    console.log("🚀 allUsers:", allUsers)
+    console.dir(allUsers, { depth: null })
+    
+
+
+  }
+
+
+
+  main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at ${port}`);
